dd25e8fe7edb2e88a28339224c691ea531387cd1
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-06-05 17:48:06 -0500

    Add: Transaction entity,dto,createTransaction and more

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,49 @@
-export class Transaction {}
+import { Product } from '../../products/entities/product.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'decimal' })
+  total: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
+  transactionDate: Date;
+
+  @OneToMany(
+    () => TransactionContents,
+    (transaction) => transaction.transaction,
+  )
+  contents: TransactionContents[];
+}
+
+@Entity()
+export class TransactionContents {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int' })
+  quantity: number;
+
+  @Column({ type: 'decimal' })
+  price: number;
+
+  @ManyToOne(() => Product, (product) => product.id, {
+    eager: true,
+    cascade: true,
+  })
+  product: Product;
+
+  @ManyToOne(() => Transaction, (transaction) => transaction.contents, {
+    cascade: true,
+  })
+  transaction: Transaction;
+}
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,11 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  Transaction,
+  TransactionContents,
+} from './entities/transaction.entity';
+import { Repository } from 'typeorm';
+import { Product } from '../products/entities/product.entity';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionRepository: Repository<Transaction>,
+    @InjectRepository(TransactionContents)
+    private readonly transactionContentsRepository: Repository<TransactionContents>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createTransactionDto: CreateTransactionDto) {
+    const transaction = new Transaction();
+    transaction.total = createTransactionDto.total;
+    await this.transactionRepository.save(transaction);
+
+    for (const contents of createTransactionDto.contents) {
+      const product = await this.productRepository.findOneBy({
+        id: contents.productId,
+      });
+
+      if (!product) {
+        throw new NotFoundException(
+          `El producto con el ID: ${contents.productId} no fue encontrado`,
+        );
+      }
+
+      if (contents.quantity > product.inventory) {
+        throw new BadRequestException(
+          `El articulo ${product.name} escede la cantidad disponible`,
+        );
+      }
+
+      product.inventory -= contents.quantity;
+
+      await this.transactionContentsRepository.save({
+        ...contents,
+        transaction,
+        product,
+      });
+    }
+
+    return 'Venta Almacenada Correctamente';
   }
 
   findAll() {
